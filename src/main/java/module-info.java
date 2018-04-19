9cbaee0bf487a61ad3440b59b5212ec6296f844d
Cendey <cendey@yeah.net>
2018-04-19 17:25:06 +0800

    Add module information to project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+/**
+ * <p>Title: science</p>
+ * <p>Description: .module-info</p>
+ * <p>Copyright: Copyright (c) 2018</p>
+ * <p>Company: MIT Edu</p>
+ *
+ * @author <chao.deng@kewill.com>
+ * @date 04/19/2018
+ * @version 1.0
+ */
+
+module mit.app.center {
+    requires java.base;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+}
