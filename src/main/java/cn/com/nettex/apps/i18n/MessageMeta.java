ec3a05dde148bdc4117c36f4a507e1ad3c89e4a7
Cendey <cendey@yeah.net>
2018-05-10 17:54:37 +0800

    Classify the i18n message to a new class.

---
+++ src/main/java/cn/com/nettex/apps/i18n/MessageMeta.java
@@ -0,0 +1,32 @@
+package cn.com.nettex.apps.i18n;
+
+/**
+ * <p>Title: science</p>
+ * <p>Description: cn.com.nettex.apps.i18n.MessageMeta</p>
+ * <p>Copyright: Copyright (c) 2018</p>
+ * <p>Company: MIT Edu</p>
+ *
+ * @author <chao.deng@kewill.com>
+ * @version 1.0
+ * @date 05/10/2018
+ */
+public class MessageMeta {
+
+    //application message configuration meta
+    public static final String WINDOWS_TITLE = "window.title";
+    public static final String MESSAGE_VIEW_FILE = "message.view.file";
+    public static final String MESSAGE_VIEW_DIRECTORY = "message.view.directory";
+    public static final String MESSAGE_BATCH_CONVERSION = "message.batch.conversion";
+    public static final String MESSAGE_SINGLE_CONVERSION = "message.single.conversion";
+    public static final String MESSAGES_MESSAGE = "messages.message";
+    public static final String MESSAGE_SOURCE_DIRECTORY_REQUIRED = "message.source.directory.required";
+    public static final String MESSAGE_SOURCE_DIRECTORY_NOT_EXIST = "message.source.directory.not.exist";
+    public static final String MESSAGE_SOURCE_NAME_REQUIRED = "message.source.name.required";
+    public static final String MESSAGE_TARGET_DIRECTORY_NOT_EXIST = "message.target.directory.not.exist";
+    public static final String MESSAGE_TARGET_FILE_FORMAT_REQUIRED = "message.target.file.format.required";
+    public static final String MESSAGE_VALIDATE_STATUS_SUCCESS = "message.validate.status.success";
+    public static final String MESSAGE_SOURCE_FILE_REQUIRED = "message.source.file.required";
+    public static final String MESSAGE_SOURCE_FILE_NOT_EXIST = "message.source.file.not.exist";
+    public static final String MESSAGE_SOURCE_FILE_NAME_ILLEGAL = "message.source.file.name.illegal";
+    public static final String MESSAGE_TARGET_FILE_NAME_ILLEGAL = "message.target.file.name.illegal";
+}
